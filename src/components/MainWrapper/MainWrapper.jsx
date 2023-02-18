d4bb0b03380aa2cd88d5617089a5713447954e3a
Nikita Voskoboinikov <nikitavoskoboinikov2828@gmail.com>
2023-02-18 20:50:27 +0200

    first commit

---
+++ src/components/MainWrapper/MainWrapper.jsx
@@ -0,0 +1,6 @@
+import { WrapperStyled } from "./styled";
+
+const MainWrapper = () => {
+  return <WrapperStyled>MainWrapper</WrapperStyled>;
+};
+export default MainWrapper;
---
+++ src/components/MainWrapper/styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const WrapperStyled = styled.div`
+  height: 100vh;
+`;
